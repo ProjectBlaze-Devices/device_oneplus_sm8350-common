1dd9cc6a4d21abd17f10303f67c3cb49d6352cde
vimal2607 <mvimal2607@gmail.com>
2024-02-27 15:27:00 +0000

    sm8350-common: Welcome to Blaze!!

--- common.mk
+++ common.mk
@@ -308,7 +308,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-blaze
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
